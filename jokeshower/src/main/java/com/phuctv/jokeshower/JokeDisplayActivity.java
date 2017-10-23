705faa0ae14e23fcbb62259313bea545bc7da5e3
Phuc Tran <phuctran@Phucs-MacBook-Pro.local>
2017-10-23 15:42:32 +0700

    add backbutton

--- jokeshower/src/main/java/com/phuctv/jokeshower/JokeDisplayActivity.java
+++ jokeshower/src/main/java/com/phuctv/jokeshower/JokeDisplayActivity.java
@@ -2,6 +2,7 @@ package com.phuctv.jokeshower;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.TextView;
 
 
@@ -12,8 +13,26 @@ public class JokeDisplayActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_joke_display);
+
+        getSupportActionBar().setDisplayShowTitleEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+
         String joke = getIntent().getStringExtra(ARGS_JOKE);
         TextView textViewJoke = (TextView) findViewById(R.id.tv_joke);
         textViewJoke.setText(joke);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                // app icon in action bar clicked; goto parent activity.
+                this.finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
